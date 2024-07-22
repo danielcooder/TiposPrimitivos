7ca06982dc31ab836ec982a03d208682fd4d0558
daniel cooder <danielcooder@gmail.com>
2024-07-22 16:29:28 -0300

    feat: tipos primitivos

---
+++ src/main/java/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,20 @@
+
+package tiposprimitivos;
+
+public class TiposPrimitivos {
+
+
+    public static void main(String[] args) {
+
+        String nome = "Daniel Oliveira";
+        float nota = 8.5f;
+
+        System.out.printf("A nota de %s é %.1f", nome, nota);
+
+    }
+
+
+
+
+
+}
